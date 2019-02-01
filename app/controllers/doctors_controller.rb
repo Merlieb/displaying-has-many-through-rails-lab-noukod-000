65cfd1db71e863067c0346e1e2c90066f95ac568
Merline Belizaire <merline@noukod.com>
2019-02-01 17:46:31 +0000

    Automatically backed up by Learn

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,16 +1,23 @@
 class DoctorsController < ApplicationController
-  before_action :set_doctor, only: [:show]
-
-  def index
-    @doctors = Doctor.all
-  end
-
   def show
-  end
-
-  private
-
-  def set_doctor
-    @doctor = Doctor.find params[:id]
+    @doctor = Doctor.find(params[:id])
   end
 end
+
+
+# class DoctorsController < ApplicationController
+#   before_action :set_doctor, only: [:show]
+# 
+#   def index
+#     @doctors = Doctor.all
+#   end
+# 
+#   def show
+#   end
+# 
+#   private
+# 
+#   def set_doctor
+#     @doctor = Doctor.find params[:id]
+#   end
+# end
