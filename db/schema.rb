30af934dad67f8ce8a6c7f15c678b9a3a5ce3497
Merline Belizaire <merline@noukod.com>
2019-02-01 17:37:27 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -35,11 +35,4 @@ ActiveRecord::Schema.define(version: 20190201151737) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
